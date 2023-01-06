de808cb41ec04948e48915d1cd8ac6928ae6cea3
Abhishek Pandey <abhishek.pandey@1Rintern.local>
2023-01-06 02:00:08 -0800

    javaapp01

---
+++ src/inheritance/Service.java
@@ -0,0 +1,6 @@
+package inheritance;
+
+public interface Service {
+	
+	public void simpleAccount();
+}
---
+++ src/inheritance/ServiceImpl.java
@@ -0,0 +1,14 @@
+package inheritance;
+
+public class ServiceImpl implements Service {
+
+	@Override
+	public void simpleAccount() {
+		System.out.println("platanium Account");
+	}
+
+	public static void main(String[] args) {
+		ServiceImpl service = new ServiceImpl();
+		service.simpleAccount();
+	}
+}
